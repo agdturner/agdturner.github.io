dcc3fa965460c3b1e0d50f51e365728cb59f6aea
Andy Turner <a.g.d.turner@leeds.ac.uk>
2022-09-18 23:13:47 +0100

    Added some styling:wq

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # agdturner.github.io
 A repository for a Website hosted on GitHub and available via the following URL:
 https://agdturner.github.io
-The website is very basic and was generated using Java in the following repository:
+
+## Contents
+index.html - The default home page loaded.
+index2.md - An alternative home page written in Markdown instead of HTML
+### courses
+A directory for courses.
+### css
+A directory for Cascading Style sheets.
+### images
+A directory for images.
+### scripts
+A directory for javascript scripts for the website.
+### tools
+A directory for third party tools for the website. These contain a mix of css and javascript.
+
+
+The website was generated using Java in the following repository:
 https://github.com/agdturner/agdturner.github.io.generator
+
+
---
+++ scripts/style.js
@@ -0,0 +1,90 @@
+/**
+ * Copyright 2022 Centre for Computational Geography.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Andy Turner
+ */
+ 
+ /**
+ * Function to initialise the style sheet for code.
+ */
+function initStyleSheet() {
+    console.log('initStyleSheet()');
+	var dark_theme = "../../../../tools/highlight/styles/github-dark.min.css";
+	var light_theme = "../../../../tools/highlight/styles/github.min.css";
+	var theme = localStorage.getItem("theme");
+	console.log('theme=' + theme);
+	if (theme == null) {
+		console.log('theme == null');
+		theme = light_theme;
+		localStorage.setItem("theme", light_theme);
+		localStorage.setItem("antitheme", dark_theme);
+		localStorage.setItem("theme_name", "Light Mode");
+		localStorage.setItem("antitheme_name", "Dark Mode");
+	} else {
+		var element = document.body;
+		element.classList.add("dark-mode");
+		if (theme === dark_theme) {
+			console.log('theme === dark_theme');
+		} else {
+			console.log('theme === light_theme');
+			element.classList.toggle("dark-mode");
+		}
+	}
+	console.log('theme=' + theme);
+	console.log('antitheme=' + localStorage.getItem("antitheme"));
+	document.getElementById('code_theme').setAttribute('href', theme);
+	document.getElementById("style_button").innerHTML = localStorage.getItem("antitheme_name");
+}
+
+/**
+ * Function to change from light mode to dark mode and vice versa.
+ */
+function swapStyleSheet(){
+	console.log('swapStyleSheet');
+	var element = document.body;
+	element.classList.toggle("dark-mode");
+	var theme = localStorage.getItem("antitheme");
+	var antitheme = localStorage.getItem("theme");
+	var theme_name = localStorage.getItem("antitheme_name");
+	var antitheme_name = localStorage.getItem("theme_name");
+	localStorage.setItem("theme", theme);
+	localStorage.setItem("antitheme", antitheme);
+	localStorage.setItem("theme_name", theme_name);
+	localStorage.setItem("antitheme_name", antitheme_name);
+	document.getElementById('code_theme').setAttribute('href', theme);
+	console.log('theme=' + theme);
+	console.log('antitheme=' + antitheme);
+	document.getElementById("style_button").innerHTML = antitheme_name;
+}
+
+addEventListener('DOMContentLoaded', (event) => {
+	initStyleSheet();
+	console.log('The DOM is fully loaded.');
+});
+
+addEventListener('load', (event) => {
+	console.log('The page is fully loaded.');
+});
+
+//addEventListener('beforeunload', (event) => {
+//	// show the confirmation dialog
+//	event.preventDefault();
+//	// Google Chrome requires returnValue to be set.
+//	event.returnValue = '';
+//});
+//
+//addEventListener('unload', (event) => {
+//	// send analytic data
+//});
